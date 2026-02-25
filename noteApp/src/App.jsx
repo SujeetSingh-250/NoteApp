304c2bd3bd6076d0c6478fd7340b08921fe8279f
SujeetSingh250 <your-email@example.com>
2026-02-25 18:07:01 +0530

    main

--- noteApp/src/App.jsx
+++ noteApp/src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
 
   const [title, setTitle] = useState('')
   const [details, setDetails] = useState('')
+  const [editIndex, setEditIndex] = useState(null);
   const [task, setTask] = useState([])
 
   const submitHandler = (e) => {
@@ -13,11 +14,19 @@ const App = () => {
     console.log(title);
     console.log(details);
 
-    const coptTask = [...task]
-    coptTask.push({ title, details })
-    setTask(coptTask);
-    console.log(coptTask);
+    const copyTask = [...task]
+    // copyTask.push({ title, details })
+    setTask(copyTask);
+    // console.log(copyTask);
 
+    if (editIndex !== null) {
+      // update existing note
+      copyTask[editIndex] = { title, details };
+      setEditIndex(null);
+    } else {
+      // add new note
+      copyTask.push({ title, details });
+    }
 
     setTitle('')
     setDetails('')
@@ -25,9 +34,15 @@ const App = () => {
 
   const deleteNote = (idx) => {
     const copyTask = [...task]
-    copyTask.splice(idx,1)
+    copyTask.splice(idx, 1)
     setTask(copyTask);
   }
+
+  const editNote = (idx) => {
+    setTitle(task[idx].title);
+    setDetails(task[idx].details);
+    setEditIndex(idx);
+  };
 
   return (
     <div className="h-screen lg:flex bg-black text-white ">
@@ -67,16 +82,23 @@ const App = () => {
         <div className="flex flex-wrap items-start justify-start gap-5 mt-5 h-full overflow-auto">
           {task.map(function (elem, idx) {
             return <div key={idx}
-              className="h-52 w-40 bg-cover flex flex-col justify-between items-start rounded-2xl bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png')] text-black p-4">
+              className="h-52 w-44 bg-cover flex flex-col justify-between items-start rounded-2xl bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png')] text-black p-4">
               <div>
                 <h1 className="leading-tight underline text-xl font-bold">{elem.title}:-</h1>
                 <p className="leading-tight font-light  px-2 text-gray-400">{elem.details}</p>
               </div>
-              <button 
-              onClick={() => {
-                deleteNote(idx)
-              }}
-              className="w-full cursor-pointer active:scale-95 bg-red-500 py-1 text-xs rounded font-bold text-white">Delete</button>
+              <div className="flex justify-between gap-5">
+                <button
+                  onClick={() => {
+                    deleteNote(idx)
+                  }}
+                  className="px-3 cursor-pointer active:scale-95 bg-red-500 py-1 text-xs rounded font-bold text-white">Delete</button>
+                <button
+                  onClick={() => {
+                    editNote(idx)
+                  }}
+                  className="px-3 cursor-pointer active:scale-95 bg-red-500 py-1 text-xs rounded font-bold text-white">Edit</button>
+              </div>
             </div>
           })}
 
